a9569dc2ea3e395b7328e9adbb34a15b9b28bf05
Lior Atari <134922718+LiorAtari@users.noreply.github.com>
2024-09-21 22:26:27 +0300

    Removing unneeded comment

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -23,8 +23,6 @@ module "eks" {
 eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
     instance_types = ["t3.large"]
-
-    # attach_cluster_primary_security_group = true
 }
 
   eks_managed_node_groups = {
